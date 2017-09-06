2f6ce73c0221d4edf3e37914a0652500964118a3
vijayaprabha707 <30829618+vijayaprabha707@users.noreply.github.com>
2017-09-06 16:51:33 +0530

    Create vv14.c

---
+++ vv14.c
@@ -0,0 +1,17 @@
+#include <stdio.h> 
+int main()
+{  
+    int counter; 
+    printf("Even numbers between 1 to 100");  
+    for(counter = 1; counter <= 100; counter++) 
+    {  
+  
+        if(counter%2 == 0) 
+        { 
+        
+            printf("%d ", counter);  
+        }  
+    }  
+   
+    return 0;  
+} 
