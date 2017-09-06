0eee041ec981d5d5263abd1dcb6697c08d1d1786
vijayaprabha707 <30829618+vijayaprabha707@users.noreply.github.com>
2017-09-06 16:22:01 +0530

    Create vv10.c

---
+++ vv10.c
@@ -0,0 +1,17 @@
+
+# include <stdio.h> 
+# include <conio.h> 
+void main() 
+{ 
+ int n, count = 0 ; 
+ clrscr();
+ printf("Enter a number");
+ scanf("%d", &n);
+ while(n > 0) 
+ { 
+  count++ ; 
+  n = n / 10 ; 
+ } 
+ printf("The number of digits is %d", count); 
+ getch();
+}
