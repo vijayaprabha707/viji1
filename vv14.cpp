5bb1223ef6ebc54c085fe9083db0732a06af8829
vijayaprabha707 <30829618+vijayaprabha707@users.noreply.github.com>
2017-09-06 16:44:55 +0530

    Create vv14.cpp

---
+++ vv14.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+ int a;
+ int g;
+ cout<<"enter number"<<endl;
+ cin>>a;
+ cout<<"enter number"<<endl;
+ cin>>g;
+ cout<<"*********************************"<<endl;
+ cout<<"odd numbers "<<endl;
+ cout<<"**********************************"<<endl;
+ for(a<=g;)
+ {
+ cout<<a<<endl;
+ a=a+2;
+ }
+}
+
